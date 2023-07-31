cbfde75f16f3cda50a18d7bc5df3dc2891bff28e
cjy0324 <1669215387@qq.com>
2023-07-31 09:53:03 +0800

    试用已完成

--- js/cjykuwan.js
+++ js/cjykuwan.js
@@ -1,29 +1,8 @@
-//  // console.log(res.data.indexData.data);
-//  var arr = res.data.indexData.data
-//  console.log(arr);
-//  //  找ul
-//  var ul_ = document.getElementsByClassName('jiekou')[0];
-//  // console.log(ul_);
-//  //   创建一个li
-//  var li_ = document.createElement('li');
-//  //  把li添加到ul后边
-//  ul_.appendChild(li_);
-//  // console.log(ul_);
-
-//  var str = '';
-//  for (var i = 0; i < arr.length; i++) {
-
-//      // console.log(i);
-//      // console.log(arr[i].goUrl);
-
-//      str += `<li><a href="${arr[i].goUrl}">${arr[i].txt} </a></Li>`;
-//  }
-
-//  ul_.innerHTML = str;
-
 
 // 排他法
-var yao1 = document.getElementsByClassName('bao');
+
+var yao1 = document.getElementsByTagName('a');
+console.log(yao1);
 var box2 = document.getElementsByClassName('zuixin');
 // console.log(box2);
 
@@ -40,10 +19,10 @@ for (var i = 0; i < yao1.length; i++) {
 
         // console.log(index);
 
-        // for (var j = 0; j < a.length; j++) {
-        //     a[j].className = '';
-        //     a[index].className = 'nei';
-        // }
+        for (var j = 0; j < yao1.length; j++) {
+            yao1[j].className = '';
+            yao1[index].className = 'nei';
+        }
         for (var k = 0; k < box2.length; k++) {
             box2[k].style.display = 'none';
             box2[index].style.display = 'block';
@@ -57,26 +36,26 @@ var zuixin = document.querySelector('.zuixin')
 var jiaright = document.querySelector('.jiazai1')
 
 jiaright.onclick = function () {
-    zuixin.style.height = '2000px';
+    zuixin.style.height = '2100px';
     jiaright.style.display = 'none';
 }
 
 
-// 点击事件
+// // 点击事件
 
-var bao = document.querySelector('.bao');
+// var bao = document.querySelector('.bao');
 
-//  为每一个盒子添加点击事件
+// //  为每一个盒子添加点击事件
 
-bao.forEach(box => {
-    box.addEventlistener('click', () => {
+// bao.forEach(box => {
+//     box.addEventlistener('click', () => {
 
-        // 移除所有盒子的边框
-        bao.forEach(box => {
-            box.classlist.remove('bao');
-        });
-        //  为当前点击的盒子添加边框样式
-        box.classlist.add('bao');
-    })
+//         // 移除所有盒子的边框
+//         bao.forEach(box => {
+//             box.classlist.remove('bao');
+//         });
+//         //  为当前点击的盒子添加边框样式
+//         box.classlist.add('bao');
+//     })
 
-});
+// });
---
+++ js/ctry.js
@@ -0,0 +1,83 @@
+// 
+
+
+// 接口
+// axios.get('')
+
+// 排他法
+// 1
+// var trial = document.getElementsByClassName('trial');
+// var trial = document.querySelector('.trial');
+
+
+// var shoufa = document.querySelector('.')
+
+// 外部排他法1
+var a1 = document.getElementsByTagName('a');
+// console.log(a1);
+var shoufa = document.getElementsByClassName('shoufa');
+// console.log(shoufa);
+
+for (var i = 0; i < a1.length; i++) {
+
+    a1[i].setAttribute('index', i);
+
+    a1[i].onclick = function () {
+        // console.log(this);
+
+        var index = this.getAttribute('index');
+        // console.log(index);
+
+        // console.log(index);
+
+        for (var j = 0; j < a1.length; j++) {
+            a1[j].className = '';
+            a1[index].className = 'nei';
+        }
+        for (var k = 0; k < shoufa.length; k++) {
+            shoufa[k].style.display = 'none';
+            shoufa[index].style.display = 'block';
+        }
+    }
+}
+
+// 内部排他法
+
+var a2 = document.getElementsByTagName('a');
+// console.log(a2);
+
+var port = document.getElementsByClassName('port');
+console.log(port);
+var arry = [a2[2], a2[3], a2[4], a2[5]]
+
+for (var i = 0; i < arry.length; i++) {
+    arry[i].setAttribute('index', i)
+    // console.log(arry[i]);
+    arry[i].onclick = function () {
+        for (var j = 0; j < arry.length; j++) {
+            port[j].style.display = 'none'
+            port[this.getAttribute('index')].style.display = 'block'
+        }
+    }
+}
+
+
+
+// 加载更多点击事件
+
+var hui = document.querySelector('.port');
+console.log(hui);
+var jia = document.querySelector('.jia');
+
+jia.onclick = function () {
+    hui.style.height = '1900px';
+    jia.style.display = 'none';
+}
+
+
+// 申请接口
+
+// var prot1 = document.getElementsByClassName('port')[1];
+// console.log(prot1);
+
+
